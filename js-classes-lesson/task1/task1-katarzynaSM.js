243c207bcb44ece498bc90db1fd6d9fc922dc0c4
katsm <k.smiglak.marczyk@gmail.com>
2022-08-23 19:43:39 +0200

    finish the tasks

---
+++ js-classes-lesson/task1/task1-katarzynaSM.js
@@ -0,0 +1,33 @@
+class Cars {
+	constructor (brand, speed){
+		this.brand = brand;
+		this.speed = speed;
+	}
+	accelerate(inc){
+		this.speed += inc;
+	}
+	brake(dec){
+		this.speed-=dec;
+	}
+
+	describe(){
+		return this.brand + " running at " + this.speed + " km/h";
+	}
+}
+
+let car = new Cars("Ford", 0);
+
+console.log(car.describe());
+car.accelerate(50);
+console.log(car.describe());
+car.accelerate(100);
+console.log(car.describe());
+car.brake(25);
+console.log(car.describe());
+
+let car2 = new Cars("Ferrari", 0);
+
+car2.accelerate(200);
+console.log(car2.describe());
+car2.brake(100);
+console.log(car2.describe());
