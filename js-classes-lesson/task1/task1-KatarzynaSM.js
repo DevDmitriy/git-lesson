d1ada162b8d4aff1adf80d95ad4411cd1f5c1885
katsm <k.smiglak.marczyk@gmail.com>
2022-08-23 18:08:54 +0200

    finish task 1

---
+++ js-classes-lesson/task1/task1-KatarzynaSM.js
@@ -0,0 +1,38 @@
+// Define a car object using plain literal object.
+// We define 2 properties (brand, speed) and
+// 3 methods (accelerate, brake, describe)
+
+
+class cars {
+	constructor (brand, speed){
+		this.brand = brand;
+		this.speed = speed;
+	}
+	accelerate(inc){
+		this.speed += inc;
+	}
+	brake(dec){
+		this.speed-=dec;
+	}
+
+	describe(){
+		return this.brand + " running at " + this.speed + " km/h";
+	}
+}
+
+let car = new cars("Ford", 0);
+
+console.log(car.describe());
+car.accelerate(50);
+console.log(car.describe());
+car.accelerate(100);
+console.log(car.describe());
+car.brake(25);
+console.log(car.describe());
+
+let car2 = new cars("Ferrari", 0);
+
+car2.accelerate(200);
+console.log(car2.describe());
+car2.brake(100);
+console.log(car2.describe());
