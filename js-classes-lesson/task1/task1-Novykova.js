c03c42d824ddcce8115b3d0ad7c22f86d6013de9
Iryna Novykova <iryna.novykova@gmail.com>
2022-08-23 19:23:44 +0300

    car 1 and car 2 obj created

---
+++ js-classes-lesson/task1/task1-Novykova.js
@@ -0,0 +1,49 @@
+// Define a car object using plain literal object.
+// We define 2 properties (brand, speed) and
+// 3 methods (accelerate, brake, describe)
+
+class Car {
+
+	constructor(brand, speed) {
+		this.brand = brand;
+		this.speed = speed;
+	}
+	
+	// methods:
+
+	accelerate(amount) {
+		this.speed += amount;
+	}
+
+	brake(amount) {
+		this.speed -= amount;
+	}
+
+	status() {
+		return this.brand + " running at " + this.speed + " km/h";
+	}
+};
+
+let car1 = new Car("Ferrari", 0);
+let car2 = new Car("Ford", 0);
+
+// console log car 1
+console.log(car1.status());
+car.accelerate(50);
+console.log(car1.status());
+car.accelerate(100);
+console.log(car1.status());
+car.brake(25);
+console.log(car1.status());
+
+// console log car 2
+
+car2.accelerate(200);
+console.log(car2.status());
+car2.brake(100);
+console.log(car2.status());
+
+
+
+
+
