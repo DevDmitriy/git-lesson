ed86ac3edeced56f25ca3c5794bc41ea55feae0e
varshajain2022 <varshajain.es@gmail.com>
2022-08-23 18:35:30 +0200

    Adding task1

--- js-classes-lesson/task1/task1.js
+++ js-classes-lesson/task1/task1.js
@@ -1,63 +1,34 @@
-// Define a car object using plain literal object.
-// We define 2 properties (brand, speed) and
-// 3 methods (accelerate, brake, describe)
+class Car {
+	constructor (brand, speed){
+		this.brand = brand
+		this.speed = speed
+	}
 
-var car = {
-
-	// properties
-	brand: "Ford",
-	speed: 0,
-
-	// methods:
-
-	accelerate: function(amount) {
+	accelerate (amount) {
 		this.speed += amount;
-	},
+	}
 
-	brake: function(amount) {
-		this.speed -= amount;
-	},
-
-	status: function() {
+	brake (amount) {
+		this.speed += amount;
+	}
+	status (){
 		return this.brand + " running at " + this.speed + " km/h";
 	}
-};
 
+}
 
-// Try methods
+let car1 = new Car('Ford', 0)
+console.log(car1.status())
+car1.accelerate(50)
+console.log(car1.status())
+car1.accelerate(100)
+console.log(car1.status())
+car1.brake(25)
+console.log(car1.status())
 
-console.log(car.status());
-car.accelerate(50);
-console.log(car.status());
-car.accelerate(100);
-console.log(car.status());
-car.brake(25);
-console.log(car.status());
-
-
-// But now if we want another car we need to repeat the code...
-// That's unnecessary repetition.
-
-var car2 = {
-
-	brand: "Ferrari",
-	speed: 0,
-
-	accelerate: function(inc) {
-		this.speed += inc;
-	},
-
-	brake: function(dec) {
-		this.speed -= dec;
-	},
-
-	status: function() {
-		return this.brand + " running at " + this.speed + " km/h";
-	}
-};
+let car2 = new Car('Ferrari', 0)
 
 car2.accelerate(200);
 console.log(car2.status());
 car2.brake(100);
 console.log(car2.status());
-
