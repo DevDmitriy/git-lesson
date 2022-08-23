c8b9c25fbb8677e750673476b100a56583cc30ae
Tam Tran <tamtran2885@gmail.com>
2022-08-23 18:16:41 +0200

    add task-1-tamtran

---
+++ js-classes-lesson/task1/task1-tamtran.js
@@ -0,0 +1,39 @@
+// Define a car object using plain literal object.
+// We define 2 properties (brand, speed) and
+// 3 methods (accelerate, brake, describe)
+
+class Car {
+  constructor(brand, speed) {
+    this.brand = brand;
+    this.speed = speed;
+  }
+
+  // methods:
+  accelerate(amount) {
+    this.speed += amount;
+  }
+
+  brake(amount) {
+    this.speed -= amount;
+  }
+
+  status() {
+    return this.brand + " running at " + this.speed + " km/h";
+  }
+}
+
+const car1 = new Car("Ford", 0);
+const car2 = new Car("Ferrari", 0);
+
+console.log(car1.status());
+car1.accelerate(50);
+console.log(car1.status());
+car1.accelerate(100);
+console.log(car1.status());
+car1.brake(25);
+console.log(car1.status());
+
+car2.accelerate(200);
+console.log(car2.status());
+car2.brake(100);
+console.log(car2.status());
