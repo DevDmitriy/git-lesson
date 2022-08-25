cf582e69aca5e1c504cf43bf392eeb579a5bbdd4
Tam Tran <tamtran2885@gmail.com>
2022-08-25 16:21:57 +0200

    fix task4-tamtran

--- js-classes-lesson/task4/task4-extended-clock.js
+++ js-classes-lesson/task4/task4-extended-clock.js
@@ -2,8 +2,14 @@
 import { Clock } from "./task4.js";
 
 class ExtendedClock extends Clock {
-  constructor({ template }, precision = 1000) {
-    super({ template });
+  constructor(obj) {
+    super(obj);
+    let { precision = 1000 } = obj;
     this.precision = precision;
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
   }
 }
