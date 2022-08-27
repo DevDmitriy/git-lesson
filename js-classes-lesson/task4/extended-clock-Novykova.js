9e6a02dedfac2e67ff9752b7d3fb8c26459bf7af
Iryna Novykova <iryna.novykova@gmail.com>
2022-08-27 20:04:42 +0300

    changed precision and override parent method

--- js-classes-lesson/task4/extended-clock-Novykova.js
+++ js-classes-lesson/task4/extended-clock-Novykova.js
@@ -1,6 +1,11 @@
 class ExtendedClock extends Clock {
-    constructor(template, precision) {
+    constructor(template, precision = 1000) {
       super(template);
-      this.precision = 1000;
+      this.precision = precision;
+    }
+
+    start() {
+      this.render();
+      this.timer = setInterval(() => this.render(), this.precision);
     }
   }
